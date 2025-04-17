a82f7849aa76b455874cfcac98ef80bb19c61e35
Nabhag8848 <nabhagmotivaras76@gmail.com>
2025-04-17 11:17:16 +0530

    fix

--- src/app/components/AppRouterProviders.tsx
+++ src/app/components/AppRouterProviders.tsx
@@ -3,13 +3,9 @@ import { StrictMode } from "react";
 import { PageTitle } from "@ui/utils/PageTitle";
 import { ThemeProvider } from "@emotion/react";
 import { THEME_DARK } from "@ui/theme/dark";
-import { useSystemColorScheme } from "@ui/theme/hooks/useSystemColorSchem";
-import { THEME_LIGHT } from "@ui/theme/light";
 
 export const AppRouterProviders = () => {
-  const preferredColorScheme = useSystemColorScheme();
-  const isDarkMode = preferredColorScheme === "dark";
-  const theme = isDarkMode ? THEME_DARK : THEME_LIGHT;
+  const theme = THEME_DARK;
 
   document.documentElement.className = theme.__name__;
 
